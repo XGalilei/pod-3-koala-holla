fb78d9b8a2f9dbfdc0063ea19fe52f8e56e89804
Sarah Fuoss <sfuoss@gmail.com>
2021-09-09 16:25:31 -0500

    Added post function to router that adds the koala to the database.

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,3 +1,4 @@
+const { Router } = require('express');
 const express = require('express');
 const koalaRouter = express.Router();
 
@@ -8,6 +9,32 @@ const koalaRouter = express.Router();
 
 
 // POST
+// Post koala to the database from user inputs
+
+router.post('/', (req, res) => {
+    const koalaToAdd = req.body;
+    // test that is relayed to the database
+    const queryText =  
+    `INSERT INTO "koala" 
+    ("name", "age", "gender", "ready_for_transfer", "notes")
+    VALUES ($1, $2, $3, $4);`
+    ;
+    // values to add into query which are
+    // received from post on the client and accessed by object attribute
+    pool.query(queryText, [
+        koalaToAdd.name,
+        koalaToAdd.age,
+        koalaToAdd.gender,
+        koalaToAdd.ready_for_transfer,
+        koalaToAdd.notes,
+    ]).then(( result ) => {
+        console.log('Success in posting to koala db!');
+        res.sendStatus(200);
+    }).catch( (error ) => {
+        console.log('There was an error posting to koala db!');
+        res.sendStatus(500);
+    })
+})
 
 
 // PUT

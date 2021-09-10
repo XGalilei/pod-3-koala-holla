32b4ef42d8ac43e92b9cb459dbb9b55e12e50724
Sarah Fuoss <sfuoss@gmail.com>
2021-09-10 14:28:29 -0500

    added click event listener

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -10,18 +10,17 @@ $( document ).ready( function(){
 }); // end doc ready
 
 function setupClickListeners() {
-  $( '#viewKoalas').on('click', '.transfer-button', readyForTransfer);
   $( '#addButton' ).on( 'click', function(){
     console.log( 'in addButton on click' );
     // get user input and put in an object
     // NOT WORKING YET :(
     // using a test object
     let koalaToSend = {
-      name: 'testName',
-      age: 'testName',
-      gender: 'testName',
-      readyForTransfer: 'testName',
-      notes: 'testName',
+      name: $('#nameIn').val(),
+      age: $('#ageIn').val(),
+      gender: $('#genderIn').val(),
+      readyForTransfer: $('#readyForTransferIn').val(),
+      notes: $('#notesIn').val(),
     };
     // call saveKoala with the new obejct
     saveKoala( koalaToSend );
